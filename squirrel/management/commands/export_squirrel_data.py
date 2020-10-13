610d9af45b2c47ba0d7ca60fa68d2ebe986fea47
Karen <yg2772@columbia.edu>
2020-10-13 00:17:57 +0000

    complete the export command, ready for test.

--- squirrel/management/commands/export_squirrel_data.py
+++ squirrel/management/commands/export_squirrel_data.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
     help = 'Export data in CVS file'
 
     def add_arguments(self, parser):
-        parser.add_argument('args')
+        parser.add_argument('args', narg = '*', type=str)
 
     def handle(self, *args, **kwargs):
-        fields = Squirrel._meta.fields
         with open(args[0], 'w') as file:
             writer = csv.writer(file)
+            fields = Squirrel._meta.fields
             for i in Squirrel.objects.all():
-                row = [getattr(i, field.name) for field in fields]
-                writer.writerow(row)
+                r = [getattr(i, field.name) for field in fields]
+                writer.writerow(r)
             file.close()
