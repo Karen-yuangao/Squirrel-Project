c24fec80b9068a9c9563d5feb8f096df36c257f5
Zihao Wu <72328439+zw2694@users.noreply.github.com>
2020-10-14 11:19:30 -0700

    write views
    map/sighting views

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from .models import SquirrelCens
+from django.forms.models import model_to_dict
+from django.core import serializers
+import random
+from json import dumps
 
-# Create your views here.
+def map(request):
+    positions = SquirrelCens.objects.all()[0:100]
+    #print(positions[:])
+    mapdata = dumps(serializers.serialize("json",list(positions), fields=('latitude', 'longitude')))
+    #print(mapdata)
+    context = {'data': mapdata}
+    return render(request,'map.html',context)
+    
+def add(request):
+    return render(request,'add.html')
+
+def sightings(request):
+    context ={} 
+    context["squirrel_profile"] = SquirrelCens.objects.all() 
+    return render(request,'sightings.html',context)
