7276aac24d7896c03fa295a1d6ac28208f69ef71
Karen <yg2772@columbia.edu>
2020-10-15 18:03:48 +0000

    update view and add form in

--- squirrel/views.py
+++ squirrel/views.py
@@ -9,52 +9,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import HttpResponseNotFound
 
 
-
 def map(request):
-    sightings = Squirrel.objects.all()[0:100]
-    context = {'sightings': sightings}
+    sightings = Squirrel.objects.all()[:100]
+    context = {
+            'sightings': sightings
+    }
     return render(request,'map.html',context)
     
 def add(request):
-    return render(request,'add.html')
+     if request.method == "POST":
+        form= SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings')
+     else:
+        form = SquirrelForm()
+        context ={
+              'form':form,
+        }
+    return render(request,'add.html',context)
 
 def sightings(request):
-    context ={} 
-    context["squirrel_profile"] = Squirrel.objects.all() 
-    return render(request,'sightings.html',context)
-
+    squirrel = Squirrel.objects.all()
+    context = {
+            'squirrels': squirrel,
+    }
+    return render(request, 'sightings.html',context)
     
-def doadd(request):
-    if request.method == "POST":
-        Squirrel.objects.create(
-            latitude = request.POST.get('latitude',None),
-            longitude = request.POST.get('longitude',None),
-            unique_squirrel_id = request.POST.get('unique_squirrel_id',None),
-            shift = request.POST.get('shift',None),
-            date = request.POST.get('date',None),
-            age = request.POST.get('age',None),
-            primary_fur_color = request.POST.get('primary_fur_color',None),
-            location = request.POST.get('location',None),
-            specific_location = request.POST.get('specific_location',None),
-            running = request.POST.get('running',None),
-            chasing = request.POST.get('chasing',None),
-            climbing = request.POST.get('climbing',None),
-            eating = request.POST.get('eating',None),
-            foraging = request.POST.get('foraging',None),
-            other_activities = request.POST.get('other_activities',None),
-            kuks = request.POST.get('kuks',None),
-            quaas = request.POST.get('quaas',None),
-            moans = request.POST.get('moans',None),
-            tail_flags = request.POST.get('tail_flags',None),
-            tail_twitches = request.POST.get('tail_twitches',None),
-            approaches = request.POST.get('approaches',None),
-            indifferent = request.POST.get('indifferent',None),
-            runs_from = request.POST.get('runs_from',None)
-		)
-        context ={}
-        context["squirrel_profile"] = Squirrel.objects.all() 
-        return render(request,'sightings.html', context)
-
 def sightingsforupdate(request,uniqueSquirrelID,param):
     print('param:',uniqueSquirrelID,param)
     profile = Squirrel.objects.get(unique_squirrel_id=uniqueSquirrelID)
